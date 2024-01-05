01e46e6525d05e0a069c03bbb681b0ec7e089f40
peter.zhang <pythias@portkey.finance>
2024-01-05 20:16:39 +0800

    111111

--- src/app/portkey/layout.tsx
+++ src/app/portkey/layout.tsx
@@ -1,4 +1,5 @@
 "use client";
+import AntdRegistry from "@/components/AntdRegistry";
 import Script from "next/script";
 
 export default function RootLayout({
@@ -11,7 +12,9 @@ export default function RootLayout({
       <head>
         <Script src="https://telegram.org/js/telegram-web-app.js" />
       </head>
-      <body>{children}</body>
+      <body>
+        <AntdRegistry>{children}</AntdRegistry>
+      </body>
     </html>
   );
 }
