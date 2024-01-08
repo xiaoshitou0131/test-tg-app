6362756328e397ecc639ade6a3defb25c677f391
peter.zhang <pythias@portkey.finance>
2024-01-08 14:16:37 +0800

    rewrite 改写为rewrite

--- next.config.js
+++ next.config.js
@@ -1,12 +1,16 @@
 const rewrites = require("./rewrites");
+const redirects = require("./redirects");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   consoleLog: true,
   crossOrigin: "anonymous",
-  async rewrites() {
-    console.log("rewrites: ", rewrites);
-    return rewrites;
+  // async rewrites() {
+  //   console.log("rewrites: ", rewrites);
+  //   return rewrites;
+  // },
+  async redirects() {
+    return redirects;
   },
   compiler: {
     removeConsole: false,
