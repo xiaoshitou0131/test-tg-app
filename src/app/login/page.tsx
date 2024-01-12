1dbd092a6e9e869ceeef16b940714a614319ec4e
peter.zhang <pythias@portkey.finance>
2024-01-12 16:07:07 +0800

    提交前端代码

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,119 +3,92 @@ import React, { useMemo } from "react";
 import { Button, Col, Divider, Row } from "antd";
 import { useRouter, useSearchParams } from "next/navigation";
 
+const loginList = [
+  {
+    key: "x",
+    name: "X（推特）",
+  },
+  {
+    key: "facebook",
+    name: "Facebook",
+  },
+  {
+    key: "twitch",
+    name: "Twitch",
+  },
+  {
+    key: "discord",
+    name: "Discord",
+  },
+  {
+    key: "reddit",
+    name: "Reddit",
+  },
+  {
+    key: "line",
+    name: "Line",
+  },
+  {
+    key: "kakaoTalk",
+    name: "KakaoTalk",
+  },
+  {
+    key: "tiktok",
+    name: "Tiktok",
+  },
+];
+
 export default function Sign() {
   const router = useRouter();
   const search = useSearchParams();
 
-  const [loginType, accessToken] = useMemo(() => {
-    return [search.get("loginType"), search.get("accessToken")];
-  }, [search]);
+  console.log(search.entries());
 
-  return (
-    <div>
-      <Divider />
-      <Row>
-        <Col span={3}>
-          <Button
-            onClick={() => router.push("/login/getCode/x")}
-            type="primary"
-          >
-            X（推特） 登录
-          </Button>
-        </Col>
-        <Col flex={1}>{loginType === "x" && accessToken}</Col>
-      </Row>
+  const [loginType, accessToken, status, refreshToken, tokenType] =
+    useMemo(() => {
+      return [
+        search.get("loginType"),
+        search.get("accessToken"),
+        search.get("status"),
+        search.get("refreshToken"),
+        search.get("tokenType"),
+      ];
+    }, [search]);
 
-      <Divider />
-      <Row>
-        <Col span={3}>
-          <Button
-            onClick={() => router.push("/login/getCode/facebook")}
-            type="primary"
-          >
-            Facebook 登录
-          </Button>
-        </Col>
-        <Col flex={1}>{loginType === "facebook" && accessToken}</Col>
-      </Row>
-
-      <Divider />
-      <Row>
-        <Col span={3}>
-          <Button
-            onClick={() => router.push("/login/getCode/twitch")}
-            type="primary"
-          >
-            Twitch 登录
-          </Button>
-        </Col>
-        <Col flex={1}>{loginType === "twitch" && accessToken}</Col>
-      </Row>
-
-      <Divider />
-      <Row>
-        <Col span={3}>
-          <Button
-            onClick={() => router.push("/login/getCode/discord")}
-            type="primary"
-          >
-            Discord 登录
-          </Button>
-        </Col>
-        <Col flex={1}>{loginType === "discord" && accessToken}</Col>
-      </Row>
-
-      <Divider />
-      <Row>
-        <Col span={3}>
-          <Button
-            onClick={() => router.push("/login/getCode/reddit")}
-            type="primary"
-          >
-            Reddit 登录
-          </Button>
-        </Col>
-        <Col flex={1}>{loginType === "reddit" && accessToken}</Col>
-      </Row>
-
-      <Divider />
-      <Row>
-        <Col span={3}>
-          <Button
-            onClick={() => router.push("/login/getCode/line")}
-            type="primary"
-          >
-            Line 登录
-          </Button>
-        </Col>
-        <Col flex={1}>{loginType === "line" && accessToken}</Col>
-      </Row>
-
-      <Divider />
-      <Row>
-        <Col span={3}>
-          <Button
-            onClick={() => router.push("/login/getCode/kakaoTalk")}
-            type="primary"
-          >
-            KakaoTalk 登录
-          </Button>
-        </Col>
-        <Col flex={1}>{loginType === "kakaoTalk" && accessToken}</Col>
-      </Row>
-
-      <Divider />
-      <Row>
-        <Col span={3}>
-          <Button
-            onClick={() => router.push("/login/getCode/tiktok")}
-            type="primary"
-          >
-            Tiktok 登录
-          </Button>
-        </Col>
-        <Col flex={1}>{loginType === "tiktok" && accessToken}</Col>
-      </Row>
-    </div>
+  return useMemo(
+    () =>
+      loginList.map(({ key, name }) => (
+        <>
+          <Divider />
+          <Row align="middle">
+            <Col span={3}>
+              <Button
+                onClick={() => router.push(`/login/getCode/${key}`)}
+                type="primary"
+              >
+                {name} 登录
+              </Button>
+              <Divider />
+              <Button
+                onClick={() => router.push(`/login/getCode/${key}`)}
+                type="primary"
+                disabled={!(loginType === key && accessToken)}
+              >
+                {name} 刷新token
+              </Button>
+            </Col>
+            <Col flex={1}>
+              {loginType === key && `login status: ${status}`}
+              <br />
+              {loginType === key && `accessToken：${accessToken}`}
+              <br />
+              {loginType === key && `refreshToken: ${refreshToken}`}
+              <br />
+              {loginType === key && `tokenType: ${tokenType}`}
+            </Col>
+          </Row>
+        </>
+      )),
+    [accessToken, loginType, refreshToken, router, status, tokenType]
   );
 }
